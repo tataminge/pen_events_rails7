24f4b5b5c221c9e81a637d7b5b19fc7a2bec217f
EC2 Default User <ec2-user@ip-172-31-47-223.ap-northeast-1.compute.internal>
2023-06-08 10:11:32 +0000

    add admin/public_login

--- app/controllers/public/post_events_controller.rb
+++ app/controllers/public/post_events_controller.rb
@@ -1,13 +1,21 @@
 class Public::PostEventsController < ApplicationController
+
+  def index
+
+  end
+
   def show
   end
 
-  def index
-  end
 
   def new
+    @post_event = Post_event.new
   end
 
   def edit
   end
+
+  def update
+  end
+
 end
